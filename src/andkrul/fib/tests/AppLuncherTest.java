3ecdd5b7a97b89771220898aac08b5e9e06f99ef
andrzej.krol.andkrul <Andrzej@Andrzej>
2015-04-24 10:12:06 +0200

    first test added, but fails method is not impplemented

--- src/andkrul/fib/tests/AppLuncherTest.java
+++ src/andkrul/fib/tests/AppLuncherTest.java
@@ -1,14 +1,17 @@
 package andkrul.fib.tests;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import andkrul.fib.app.AppLuncher;
 
 public class AppLuncherTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public  void checkGreatnessTest() {
+		Assert.assertEquals(50,AppLuncher.checkGreatness(50, 30));
 	}
 
+	
+	
 }
