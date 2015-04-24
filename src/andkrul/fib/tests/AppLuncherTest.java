c769c475d0da47b6311374cfd2292093735b60cc
andrzej.krol <Andrzej@Andrzej>
2015-04-24 09:39:03 +0200

    Test class added

---
+++ src/andkrul/fib/tests/AppLuncherTest.java
@@ -0,0 +1,14 @@
+package andkrul.fib.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppLuncherTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
